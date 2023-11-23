4d1730bdaada6958cb8dd15e3833ac9153199639
Favour <gismozfexie@yahoo.com>
2023-11-23 23:18:19 +0100

    chore: remove vuetify & add headless ui

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,13 @@
 import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import VueApexCharts from "vue3-apexcharts";
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-const vuetify = createVuetify({
-  components,
-  directives,
-  icons: {
-    defaultSet: 'mdi',
-    aliases,
-    sets: {
-      mdi,
-    },
-  },
-})
-
 app.use(createPinia())
 app.use(router)
-app.use(vuetify)
 app.use(VueApexCharts)
-
 app.mount('#app')
